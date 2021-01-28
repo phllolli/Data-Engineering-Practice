789999249c89cd8211a2fb9a3ef18ffab83bae5a
Phusit Somboonyingsuk <56222561+lLoLii@users.noreply.github.com>
2021-01-28 15:42:29 +0700

    Query: select and insert

--- sql_queries.py
+++ sql_queries.py
@@ -58,3 +58,49 @@ class drop_table:
     song = "DROP TABLE IF EXISTS songs"
     artist = "DROP TABLE IF EXISTS artists"
     time = "DROP TABLE IF EXISTS time"
+    all = [songplay, user, song, artist, time]
+    
+class insert_record:
+    songplay = ("""
+                INSERT INTO songplays
+                (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (songplay_id) DO NOTHING;
+                """)
+    
+    user = ("""
+            INSERT INTO users
+            (user_id, first_name, last_name, gender, level)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (user_id) DO NOTHING;
+            """)
+    
+    song = ("""
+            INSERT INTO songs
+            (song_id, title, artist_id, year, duration)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (song_id) DO NOTHING;
+            """)
+    
+    artist = ("""
+            INSERT INTO artists
+            (artist_id, name, location, latitude, longitude)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (artist_id) DO NOTHING;
+            """)
+    
+    time_table_insert = ("""
+        INSERT INTO time
+        (start_time, hour, day, week, month, year, weekday)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (start_time) DO NOTHING;
+                        """)
+
+class select:
+    song = ("""
+        SELECT song_id, artists.artist_id
+        FROM songs JOIN artists ON songs.artist_id = artists.artist_id
+        WHERE songs.title = %s
+        AND artists.name = %s
+        AND songs.duration = %s        
+            """)
